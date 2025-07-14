6eab5b00bdcdf80a42675f1d710d9e0a3e3afee8
bishal <d.bishalpaul@gmail.com>
2025-07-14 21:43:45 +0530

    h

--- src/scripts/cron.js
+++ src/scripts/cron.js
@@ -1,17 +1,18 @@
 import cron from "cron";
 import https from "https";
 import dotenv from "dotenv"
-dotenv.config(); 
+
+const API_URL = "https://taskxp.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   // Ensure process.env.API_URL is defined when deploying, e.g., for keep-alive pings.
-  if (!process.env.API_URL) {
+  if (!API_URL) {
     console.warn("API_URL environment variable is not set. Cron job might not function correctly for keep-alive.");
     return; // Exit if URL is not defined
   }
 
   https
-    .get(process.env.API_URL, (res) => {
+    .get(API_URL, (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully to keep server alive.");
       else console.log(`GET request failed with status code: ${res.statusCode}`);
     })

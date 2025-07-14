4e27c49f0c6d76a75c18fd0ccefe0397170afd59
bishal <d.bishalpaul@gmail.com>
2025-07-14 20:14:23 +0530

    hello

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,17 +6,10 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: (origin, callback) => {
-        const allowedOrigins = [
-          "http://localhost:5173",
-          "https://task-frontend-rank.vercel.app"
-        ];
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
+      origin: [
+        "http://localhost:5173",
+        "https://task-frontend-rank.vercel.app"
+      ],
       methods: ["GET", "POST"],
       credentials: true
     }
@@ -25,7 +18,6 @@ export const initSocket = (server) => {
   io.on("connection", (socket) => {
     console.log("⚡ Socket connected:", socket.id);
 
-    // Add your listeners here
     socket.on("disconnect", () => {
       console.log("🔌 Socket disconnected:", socket.id);
     });
